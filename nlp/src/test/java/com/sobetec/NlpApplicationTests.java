5d9c294e49162f9aa200bbd594ae4bc6ae6d1ee0
YUJH <YUJH@YUJH>
2020-11-11 14:41:40 +0900

    first file

---
+++ nlp/src/test/java/com/sobetec/NlpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobetec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
