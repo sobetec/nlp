721ed0656eb8005fe86829c43a5598172e65643a
dongik.kwak <dongik.kwak@sobetec.com>
2020-12-17 13:39:14 +0900

    dcsfsdfdsgafgfazsdfsdf

--- nlp/src/main/java/com/sobetec/NlpApplication.java
+++ nlp/src/main/java/com/sobetec/NlpApplication.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@EnableEncryptableProperties
 @SpringBootApplication
 public class NlpApplication {
 
---
+++ nlp/src/main/java/com/sobetec/encryript/Jasypt.java
@@ -0,0 +1,24 @@
+package com.sobetec.encryript;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+
+
+public class Jasypt {
+	public static void main(String[] args) {
+        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+        jasypt.setPassword("sobehub");      //암호화 키(password)
+        jasypt.setAlgorithm("PBEWithMD5AndDES");
+ 
+ 
+        String encryptedText = jasypt.encrypt("jdbc:log4jdbc:postgresql://116.124.69.154:5432/postgres");    //암호화
+        String plainText = jasypt.decrypt(encryptedText);  //복호화
+ 
+        System.out.println("encryptedText:  " + encryptedText); //암호화된 값
+        System.out.println("plainText:  " + plainText);         //복호화된 값
+    }
+
+
+
+
+}
