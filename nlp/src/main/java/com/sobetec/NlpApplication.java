7d8b93984a6ed6dc5eb824a702bef0a6177d4ff3
sdb <sdb@DESKTOP-FOSDG2T>
2020-12-21 16:35:01 +0900

    201221 16:34 sdb

--- nlp/src/main/java/com/sobetec/NlpApplication.java
+++ nlp/src/main/java/com/sobetec/NlpApplication.java
@@ -1,21 +1,33 @@
 package com.sobetec;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import com.sobetec.nlp.reportlist.ReportFileProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@Configuration
 @EnableEncryptableProperties
 @SpringBootApplication
 @EnableConfigurationProperties({ReportFileProperties.class})
 public class NlpApplication {
-
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NlpApplication.class, args);
+		
+	
+		
 	}
 	
 	@Bean("encryptorBean")
@@ -62,6 +74,7 @@ public class NlpApplication {
 	        }
 	    };
 	}
+	
 	
 
 }
